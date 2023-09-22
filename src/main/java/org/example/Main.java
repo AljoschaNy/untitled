5fe53a47ee08fb69b82c457953726baf3d1d72c5
Aljoscha Nyang <a.nyang@gmx.de>
2023-09-22 12:32:34 +0200

    Add jfiglet dependency

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import com.github.lalyos.jfiglet.FigletFont;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+
+        String asciiArt = FigletFont.convertOneLine("Hello world");
+        System.out.println(asciiArt);
     }
 }
