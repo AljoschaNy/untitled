00f67c610a3e16dcce6520da33b41c6bc1ddc0d7
Aljoscha Nyang <70570811+AljoschaNy@users.noreply.github.com>
2023-09-22 15:54:07 +0200

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,6 +8,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         String asciiArt = FigletFont.convertOneLine("Hello world");
+        System.out.println("Hello again");
         System.out.println(asciiArt);
     }
 }
